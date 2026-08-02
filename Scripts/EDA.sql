fab37497859e08e185042dc66161472134de7cb5
Hrushikesh Mate <otheruses022@gmail.com>
2026-08-02 16:27:02 +0100

    Add EDA SQL script for database exploration
    This SQL script explores the database structure, dimensions, date ranges, measures, and magnitude analysis. It includes queries to retrieve metadata, unique values, aggregated metrics, and distributions across various dimensions.

---
+++ Scripts/EDA.sql
@@ -0,0 +1,222 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+use Datawarehouse ;
+
+-- Retrive a list of all tables in a database 
+
+select * from INFORMATION_SCHEMA.TABLES;
+
+-- retrive all columns fora specific tables (dim_customers)
+
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME = 'dim_customers';
+
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+
+select distinct country from gold.dim_customers
+order by country;
+
+
+-- Retrieve a list of unique categories, subcategories, and products
+
+select distinct category from gold.dim_products;
+
+select category ,subcategory , product_name from gold.dim_products;
+
+/*
+===============================================================================
+Date Range Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
+
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Determine the first and last order date and the total duration in months
+select order_date from gold.fact_sales; 
+
+select 
+min(order_date) as first_order_date,
+max(order_date) as last_order_date,
+datediff(year, min(order_date),max(order_date)) as order_range_year,
+datediff(month, min(order_date),max(order_date)) as order_range_month
+from gold.fact_sales;
+
+-- Find the youngest and oldest customer based on birthdate
+select 
+min(birthdate) as oldest_birthdate,
+datediff(year, min(birthdate) , getdate()) as oldest_age,
+max(birthdate) as youngest_birthdate,
+datediff(year, max(birthdate) , getdate()) as youngest_age
+from gold.dim_customers;
+
+
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+
+-- Find the Total Sales
+select sum(sales_amount) from gold.fact_sales;
+
+-- Find how many items are sold
+select sum(quantity) from gold.fact_sales;
+
+-- Find the average selling price
+select avg(price) from gold.fact_sales; -- avg price quite high
+
+-- Find the Total number of Orders
+select count(order_number) from gold.fact_sales; -- there are 60k total order (some repeated)
+select count( distinct order_number) from gold.fact_sales; -- there are 27k distinct orders
+
+-- Find the total number of products
+select count( distinct order_number) from gold.fact_sales; -- 295 products
+
+
+-- Find the total number of customers
+select count(customer_key) from gold.dim_customers; -- total 18484 customers
+select count(distinct customer_key) from gold.dim_customers; -- same 18484
+
+
+
+-- Find the total number of customers that has placed an order
+select count(distinct customer_key ) from gold.dim_customers; -- used customer_key becoz its a PK
+
+
+-- Generate a Report that shows all key metrics of the business
+
+select 'Total Sales' as measure_name , sum(sales_amount) as measure_value from gold.fact_sales
+union all
+select 'Total Quantity' as measure_name , sum(quantity) from gold.fact_sales
+union all
+select 'Average Price' as measure_name , AVG(price) from gold.fact_sales
+union all
+select 'Total Orders' as measure_name , count( distinct order_number) from gold.fact_sales
+union all
+select 'Total Products' as measure_name , count( distinct order_number) from gold.fact_sales
+union all
+select 'Total customers' as measure_name , count(customer_key) from gold.dim_customers;
+
+
+/*
+===============================================================================
+Magnitude Analysis
+===============================================================================
+Purpose:
+    - To quantify data and group results by specific dimensions.
+    - For understanding data distribution across categories.
+
+SQL Functions Used:
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+    - GROUP BY, ORDER BY
+===============================================================================
+*/
+
+-- Find total customers by countries
+select 
+country,
+count(customer_key) as total_customers
+from gold.dim_customers
+group by country
+order by total_customers desc;
+
+-- Find total customers by gender
+select 
+gender,
+count(customer_key) as total_customers
+from gold.dim_customers
+group by gender
+order by total_customers desc;
+
+-- Find total products by category
+select category,
+count(product_key) as total_product
+from gold.dim_products
+group by category
+order by total_product desc;
+
+select category,subcategory,
+count(product_key) as total_product
+from gold.dim_products
+group by category ,subcategory
+order by total_product desc;
+ 
+
+-- What is the average costs in each category?
+select category,
+avg(cost) as avg_cost
+from gold.dim_products
+group by category
+order by avg_cost desc;
+
+
+-- What is the total revenue generated for each category?
+select category,
+sum(price) as Total_revenue
+from gold.fact_sales f
+join  gold.dim_products p on f.product_key = p.product_key 
+group by category
+order by Total_revenue;
+
+
+-- What is the total revenue generated by each customer?
+select 
+    c.customer_key ,
+    first_name,
+    last_name,
+    sum(price) as Total_revenue
+from gold.fact_sales f
+join  gold.dim_customers c on f.customer_key = c.customer_key 
+group by 
+    c.customer_key ,
+    first_name,
+    last_name
+order by Total_revenue desc;
+
+
+-- What is the distribution of sold items across countries?
+
+select 
+    c.country,
+    sum(quantity) as sold_items
+from gold.fact_sales f
+left join gold.dim_customers c on c.customer_key = f.customer_key
+group by c.country
+order by sold_items desc;
